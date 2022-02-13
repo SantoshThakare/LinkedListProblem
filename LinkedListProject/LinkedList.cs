5973d5628e457cf63313dded459169a0a521e83f
SantoshThakare <santoshthakare50@gmail.com>
2022-02-13 10:52:16 +0530

    [santosh]: Added Simple Linked List

---
+++ LinkedListProject/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProject
+{
+    class LinkedList
+    {
+        public Node head;
+        public void Add(int value)
+        {
+            Node node = new Node(value);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Added to the List", node.data);
+        }
+
+    }
+}
---
+++ LinkedListProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList Problem");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
