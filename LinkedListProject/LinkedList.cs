be6a7374e4df08f893cd9f4932ab87f3ac19600d
SantoshThakare <santoshthakare50@gmail.com>
2022-02-13 15:56:05 +0530

    [santosh]: Simple Linked List

--- LinkedListProject/LinkedList.cs
+++ LinkedListProject/LinkedList.cs
@@ -8,10 +8,10 @@ namespace LinkedListProject
 {
     class LinkedList
     {
-        public Node head;
-        public void Add(int value)
+         internal Node head;
+        internal void Add(int data)
         {
-            Node node = new Node(value);
+            Node node = new Node(data);
             if (this.head == null)
             {
                 this.head = node;
@@ -22,11 +22,121 @@ namespace LinkedListProject
                 while (temp.next != null)
                 {
                     temp = temp.next;
-
                 }
                 temp.next = node;
             }
-            Console.WriteLine("{0} Added to the List", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        internal Node RemoveLastNode()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node NewNode = head;
+            while (NewNode.next.next != null)
+            {
+                NewNode = NewNode.next;
+            }
+            NewNode.next = null;
+            return head;
+        }
+        internal int Search(int value)
+        {
+            Node node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return -1;
+        }
+        internal void DeleteNodeAtPerticularPosition(int position)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            Node temp = this.head;
+            if (position == 0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for (int i = 0; temp != null && i < position - 1; i++)
+            {
+                temp = temp.next;
+            }
+            if (temp == null || temp.next == null)
+            {
+                return;
+            }
+            Node next = temp.next.next;
+            temp.next = next;
+        }
+        internal void Size()
+        {
+            Node temp = this.head;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("Length of LinkedList :" + " " + count);
+
         }
 
     }
--- LinkedListProject/Program.cs
+++ LinkedListProject/Program.cs
@@ -11,10 +11,26 @@ namespace LinkedListProject
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the LinkedList Problem");
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
+           
+            LinkedList list = new LinkedList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+            Console.WriteLine("Provided LinkedList :");
+            list.Display();
+
+            list.Add(60);
+            list.AddInReverseOrder(1);
+            list.RemoveFirstNode();
+            list.RemoveLastNode();
+            list.Search(20);
+            int Requiredposition = list.Search(20);
+            list.DeleteNodeAtPerticularPosition(Requiredposition);
+            Console.WriteLine("The updated LinkedList :");
+            list.Size();
+
         }
     }
 }
